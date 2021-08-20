f7d16f5ad5eb4a8173c9e8f7db6b7e5a9fccafbd
Jobby-designs <62705629+Jobby-designs@users.noreply.github.com>
2021-08-20 17:14:47 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"  name="viewport" content="width=device-width,intial-scale=1.0">
+
+    <title>welcome</title>
+
+ 
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="styleresume.css">
+
+
+</head>
+<body>
+
+
+<div class="container">
+  <div class="logo">
+    <a href="https://training.zuri.team/"><img src="zuri.png"></a>
+  </div>
+  <div class="header">
+    <div class="full-name">
+      <span class="first-name">OLUWAJOBA</span> 
+      <span class="last-name">TELLA</span>
+    </div>
+    <div class="contact-info">
+      <span class="email">Email: </span>
+      <span class="email-val">goodnesstella982@gmail.com</span>
+      <span class="separator"></span>
+      <span class="phone">Phone: </span>
+      <span class="phone-val">09078681380</span>
+    </div>
+    
+    <div class="about">
+      <span class="position">Back-End Developer </span>
+      <span class="desc">
+        I am a Back End Developer with experience writing php programming language,html, css,js . I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. 
+      </span>
+    </div>
+  </div>
+   <div class="details">
+    <div class="section">
+      <div class="section__title">Work Experience</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Intern</div>
+            <div class="addr">FEMTECH IT, Kwara</div>
+            <div class="duration">Jan 2020 - June 2020</div>
+          </div>
+
+      </div>
+    </div>
+    <div class="section">
+      <br/>
+      <div class="section__title">Education</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">University of Ilorin</div>
+            <div class="addr">Ilorin, Kwara State, Nigeria</div>
+            <div class="duration">Jan 2017 - Feb 2022</div>
+          </div>
+          
+        </div>
+        
+
+      </div>
+      
+  </div>
+     <div class="section">
+      <div class="section__title">Projects</div> 
+       <div class="section__list">
+         <div class="section__list-item">
+           <div class="name">DSP</div>
+           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.</div>
+         </div>
+         
+         <div class="section__list-item">
+                    <div class="name">DSP</div>
+           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. <a href="/login">link</a>
+           </div>
+         </div>
+       </div>
+    </div>
+     <div class="section">
+       <div class="section__title">Skills</div>
+       <div class="skills">
+         
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             HTML
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             Javascript
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             PHP
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox"/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+
+       </div>
+       <div class="skills__item">
+           <div class="left"><div class="name">
+             CSS</div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+       </div>
+     <div class="section">
+     <div class="section__title">
+       Interests
+       </div>
+       <div class="section__list">
+         <div class="section__list-item">
+                  Football, reading novels, watching movies, programming.
+          </div>
+
+          <br/>
+        <hr>
+          <footer class="footer">
+
+            <p style="font-size: 20px; margin-bottom: 20px">Kindly send a message to me...</p>
+  <form method="POST" action="" enctype="multipart/form-data">
+
+  <div class="form-group">
+    <label>  Name</label>
+    <input type="text" name="fullname" placeholder="Enter Name" required="required"><br><br>
+  </div>
+
+<div class="form-group">
+  <label>Email</label>
+  <input type="email" name="email" placeholder="Enter Email Address" required="required"><br><br>
+</div>
+
+<div class="form-group">
+  <label>Subject</label>
+  <input type="text" name="subject" placeholder="Subject" required="required"><br><br>
+</div>
+
+<div class="form-group">
+  <label>Message</label>
+  <textarea name="message" rows="10" cols="50" required="required">
+  </textarea>
+</div>
+ 
+<br><br>
+
+<div class="form-group">
+  <button type="submit" name="submitButton" class="submitButton">Submit</button>
+</div>
+
+</form>
+</footer>
+
+</body>
+
+</html>
+
